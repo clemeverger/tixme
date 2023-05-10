9f131a0e61db33caef5b4c20104c65d9e8e34d50
Clément Verger <clemeverger@gmail.com>
2023-05-11 00:01:53 +0200

    Fix

--- theme/theme.tsx
+++ theme/theme.tsx
@@ -47,7 +47,39 @@ export const theme = extendTheme({
     },
   },
   components: {
-    Button: Button,
-    Input: Input,
+    Button: {
+      baseStyle: {
+        borderRadius: 'full',
+        fontWeight: 'medium',
+      },
+      variants: {
+        primary: { bg: 'primary.500', color: 'white', _focus: { bg: 'primary.600' } },
+        secondary: { bg: 'neutral.800', color: 'neutral.500', _focus: { bg: 'neutral.700' } },
+      },
+      defaultProps: {
+        variant: 'primary',
+      },
+    },
+    Input: {
+      baseStyle: {
+        field: {
+          bg: 'neutral.800',
+          color: 'white',
+          _placeholder: {
+            color: 'neutral.600',
+          },
+        },
+      },
+      variants: {
+        primary: {
+          field: {
+            borderRadius: 'full',
+          },
+        },
+      },
+      defaultProps: {
+        variant: 'primary',
+      },
+    },
   },
 })
